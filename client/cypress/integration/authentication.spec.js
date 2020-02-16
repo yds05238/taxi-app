060e2bd940d1139cfbc51316151f184ad4fcdb5c
yds05238 <jy392@cornell.edu>
2020-02-16 01:37:33 -0500

    Paths

--- client/cypress/integration/authentication.spec.js
+++ client/cypress/integration/authentication.spec.js
@@ -1,9 +1,121 @@
 describe('Authentication', function () {
     it('Can log in.', function () {
-      cy.visit('/#/log-in');
-      cy.get('input#username').type('gary.cole@example.com');
-      cy.get('input#password').type('pAssw0rd', { log: false });
-      cy.get('button').contains('Log in').click();
-      cy.hash().should('eq', '#/');
+        cy.server();
+        cy.route({
+          method: 'POST',
+          url: '**/api/log_in/**',
+          status: 200,
+          response: {
+            'access': 'ACCESS_TOKEN',
+            'refresh': 'REFRESH_TOKEN'
+          }
+        }).as('logIn');
+      
+        cy.visit('/#/log-in');
+        cy.get('input#username').type('gary.cole@example.com');
+        cy.get('input#password').type('pAssw0rd', { log: false });
+        cy.get('button').contains('Log in').click();
+      
+        cy.wait('@logIn');
+      
+        cy.hash().should('eq', '#/');
+        cy.get('button').contains('Log out');
+      });
+
+    it('Can sign up.', function () {
+        cy.visit('/#/sign-up');
+        cy.get('input#username').type('gary.cole@example.com');
+        cy.get('input#firstName').type('Gary');
+        cy.get('input#lastName').type('Cole');
+        cy.get('input#password').type('pAssw0rd', { log: false });
+        cy.get('select#group').select('driver');
+        cy.fixture('images/photo.jpg').then(photo => {
+            cy.get('input#photo').upload({
+              fileContent: photo,
+              fileName: 'photo.jpg',
+              mimeType: 'application/json'
+            });
+          });
+        cy.get('button').contains('Sign up').click();
+        cy.hash().should('eq', '#/log-in');
+      });
+
+    it('Cannot visit the login page when logged in.', function () {
+        const { username, password } = Cypress.env('credentials');
+        
+        // Log in.
+        cy.server();
+        cy.route({
+            method: 'POST',
+            url: '**/api/log_in/**',
+            status: 200,
+            response: {
+            'access': 'ACCESS_TOKEN',
+            'refresh': 'REFRESH_TOKEN'
+            }
+        }).as('logIn');
+        cy.visit('/#/log-in')
+        cy.get('input#username').type(username)
+        cy.get('input#password').type(password, { log: false })
+        cy.get('button').contains('Log in').click()
+        cy.hash().should('eq', '#/')
+        cy.get('button').contains('Log out')
+        cy.wait('@logIn')
+        
+        cy.visit('/#/log-in');
+        cy.hash().should('eq', '#/');
     });
+
+    it('Cannot visit the sign up page when logged in.', function () {
+        const { username, password } = Cypress.env('credentials');
+        cy.server();
+        cy.route({
+          method: 'POST',
+          url: '**/api/log_in/**',
+          status: 200,
+          response: {
+            'access': 'ACCESS_TOKEN',
+            'refresh': 'REFRESH_TOKEN'
+          }
+        }).as('logIn');
+        cy.visit('/#/log-in')
+        cy.get('input#username').type(username)
+        cy.get('input#password').type(password, { log: false })
+        cy.get('button').contains('Log in').click()
+        cy.hash().should('eq', '#/')
+        cy.get('button').contains('Log out')
+        cy.wait('@logIn')
+      
+        cy.visit('/#/sign-up');
+        cy.hash().should('eq', '#/');
+      });
+
+      it('Cannot see links when logged in.', function () {
+        const { username, password } = Cypress.env('credentials');
+        cy.server();
+        cy.route({
+          method: 'POST',
+          url: '**/api/log_in/**',
+          status: 200,
+          response: {
+            'access': 'ACCESS_TOKEN',
+            'refresh': 'REFRESH_TOKEN'
+          }
+        }).as('logIn');
+        cy.visit('/#/log-in')
+        cy.get('input#username').type(username)
+        cy.get('input#password').type(password, { log: false })
+        cy.get('button').contains('Log in').click()
+        cy.hash().should('eq', '#/')
+        cy.get('button').contains('Log out')
+        cy.wait('@logIn')
+      
+        cy.get('button#signUp').should('not.exist');
+        cy.get('button#logIn').should('not.exist');
+      });
+
+
+
   });
+
+  
--- client/src/components/LogIn.js
+++ client/src/components/LogIn.js
@@ -1,72 +1,73 @@
-import React, { useState } from 'react'; // changed
+import React from 'react'; 
 import { Formik } from 'formik';
 import {
   Breadcrumb, Button, Card, Col, Form, Row
 } from 'react-bootstrap';
-import { Link, Redirect } from 'react-router-dom'; // changed
+import { Link } from 'react-router-dom'; 
 
 function LogIn (props) {
-    const [isSubmitted, setSubmitted] = useState(false);
+    const onSubmit = async (values, actions) => {
+        try {
+          await props.logIn(values.username, values.password);
+        }
+        catch (error) {
+          console.error(error);
+        }
+    };
+    
 
-    // new
-    const onSubmit = (values, actions) => setSubmitted(true);
-
-    // new
-    if (isSubmitted) {
-        return <Redirect to='/' />;
-    }
-  return (
-    <Row>
-      <Col lg={12}>
-        <Breadcrumb>
-          <Breadcrumb.Item href='/'>Home</Breadcrumb.Item>
-          <Breadcrumb.Item active>Log in</Breadcrumb.Item>
-        </Breadcrumb>
-        <Card>
-          <Card.Header>Log in</Card.Header>
-          <Card.Body>
-          <Formik
-            initialValues={{
-                username: '',
-                password: ''
-            }}
-            onSubmit={onSubmit}
-            >
-            {({
-                handleChange,
-                handleSubmit,
-                values
-            }) => (
-                <Form noValidate onSubmit={handleSubmit}>
-                <Form.Group controlId='username'>
-                    <Form.Label>Username:</Form.Label>
-                    <Form.Control
-                    name='username'
-                    onChange={handleChange}
-                    value={values.username}
-                    />
-                </Form.Group>
-                <Form.Group controlId='password'>
-                    <Form.Label>Password:</Form.Label>
-                    <Form.Control
-                    name='password'
-                    onChange={handleChange}
-                    type='password'
-                    value={values.password}
-                    />
-                </Form.Group>
-                <Button block type='submit' variant='primary'>Log in</Button>
-                </Form>
-            )}
-            </Formik>
-          </Card.Body>
-          <p className='mt-3 text-center'>
-            Don't have an account? <Link to='/sign-up'>Sign up!</Link>
-          </p>
-        </Card>
-      </Col>
-    </Row>
-  );
+    return (
+        <Row>
+        <Col lg={12}>
+            <Breadcrumb>
+            <Breadcrumb.Item href='/'>Home</Breadcrumb.Item>
+            <Breadcrumb.Item active>Log in</Breadcrumb.Item>
+            </Breadcrumb>
+            <Card>
+            <Card.Header>Log in</Card.Header>
+            <Card.Body>
+            <Formik
+                initialValues={{
+                    username: '',
+                    password: ''
+                }}
+                onSubmit={onSubmit}
+                >
+                {({
+                    handleChange,
+                    handleSubmit,
+                    values
+                }) => (
+                    <Form noValidate onSubmit={handleSubmit}>
+                    <Form.Group controlId='username'>
+                        <Form.Label>Username:</Form.Label>
+                        <Form.Control
+                        name='username'
+                        onChange={handleChange}
+                        value={values.username}
+                        />
+                    </Form.Group>
+                    <Form.Group controlId='password'>
+                        <Form.Label>Password:</Form.Label>
+                        <Form.Control
+                        name='password'
+                        onChange={handleChange}
+                        type='password'
+                        value={values.password}
+                        />
+                    </Form.Group>
+                    <Button block type='submit' variant='primary'>Log in</Button>
+                    </Form>
+                )}
+                </Formik>
+            </Card.Body>
+            <p className='mt-3 text-center'>
+                Don't have an account? <Link to='/sign-up'>Sign up!</Link>
+            </p>
+            </Card>
+        </Col>
+        </Row>
+    );
 }
 
 export default LogIn;
