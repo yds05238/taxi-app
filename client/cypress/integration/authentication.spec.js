3606e04050b31c03dfd60c14e0e1d2237105ab4d
yds05238 <jy392@cornell.edu>
2020-02-16 00:27:08 -0500

    login

--- client/cypress/integration/authentication.spec.js
+++ client/cypress/integration/authentication.spec.js
@@ -15,3 +15,14 @@ it('Can sign up.', function () {
     cy.get('button').contains('Sign up').click();
     cy.hash().should('eq', '#/log-in');
   });
+
+  it('Can log in.', function () {
+    cy.visit('/#/log-in');
+    cy.get('input#username').type('gary.cole@example.com');
+    cy.get('input#password').type('pAssw0rd', { log: false });
+    cy.get('button').contains('Log in').click();
+    cy.hash().should('eq', '#/');
+
+    cy.get('button').contains('Log out');
+  });
+  
--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,7 @@
-import React from 'react';
-import { Container, Navbar } from 'react-bootstrap'; 
+import React, { useState } from 'react'; 
+import {
+  Button, Container, Form, Navbar
+} from 'react-bootstrap'; 
 import { LinkContainer } from 'react-router-bootstrap';
 import { Link, Route, Switch } from 'react-router-dom';
 
@@ -9,14 +11,25 @@ import LogIn from './components/LogIn';
 import './App.css';
 
 function App () {
+  const [isLoggedIn, setLoggedIn] = useState(false);
+
+  const logIn = (username, password) => setLoggedIn(true);
+
   return (
-    <>
+    <div>
       <Navbar bg='light' expand='lg' variant='light'>
         <LinkContainer to='/'>
           <Navbar.Brand className='logo'>Taxi</Navbar.Brand>
         </LinkContainer>
         <Navbar.Toggle />
-        <Navbar.Collapse></Navbar.Collapse>
+        <Navbar.Collapse>
+          {
+            isLoggedIn &&
+            <Form inline className='ml-auto'>
+              <Button type='button'>Log out</Button>
+            </Form>
+          }
+        </Navbar.Collapse>
       </Navbar>
       <Container className='pt-3'>
         <Switch>
@@ -28,10 +41,12 @@ function App () {
             </div>
           )} />
           <Route path='/sign-up' component={SignUp} />
-          <Route path='/log-in' component={LogIn} />
+          <Route path='/log-in' render={() => (
+            <LogIn logIn={logIn} />
+          )} />
         </Switch>
       </Container>
-    </>
+    </div>
   );
 }
 
