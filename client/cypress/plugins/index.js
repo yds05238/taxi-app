6a085448f9ab56f0e24ca532ee99a5f92e792650
yds05238 <jy392@cornell.edu>
2020-02-16 03:36:49 -0500

    Cypress tasks

--- client/cypress/plugins/index.js
+++ client/cypress/plugins/index.js
@@ -1,24 +1,31 @@
 const knex = require('knex');
 
+
 module.exports = (on, config) => {
-  on('task', {
-    async tableInsert ({ table, rows, truncate }) {
-      const client = await knex({
-        client: 'pg',
-        connection: config.env.database
-      });
-      if (truncate) {
-        await client.raw(`TRUNCATE ${table} RESTART IDENTITY CASCADE`);
+    on('task', {
+      async tableTruncate ({ table }) {
+        const client = await knex({
+          client: 'pg',
+          connection: config.env.database
+        });
+        return client.raw(`TRUNCATE ${table} RESTART IDENTITY CASCADE`);
+      },
+      async tableInsert ({ table, rows, truncate }) {
+        const client = await knex({
+          client: 'pg',
+          connection: config.env.database
+        });
+        if (truncate) {
+          await client.raw(`TRUNCATE ${table} RESTART IDENTITY CASCADE`);
+        }
+        return client.insert(rows, ['id']).into(table);
+      },
+      async tableSelect ({ table }) {
+        const client = await knex({
+          client: 'pg',
+          connection: config.env.database
+        });
+        return client.select().table(table);
       }
-      return client.insert(rows, ['id']).into(table);
-    },
-    
-    async tableSelect ({ table }) {
-      const client = await knex({
-        client: 'pg',
-        connection: config.env.database
-      });
-      return client.select().table(table);
-    }
-  });
-}
+    });
+  }
